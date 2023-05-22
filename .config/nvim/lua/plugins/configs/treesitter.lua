c35bbdec647a6e89f643c38c967793085d009eed
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2023-05-22 15:42:58 +0300

    Change folder structure

---
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua" , "rust", "javascript", "typescript" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
